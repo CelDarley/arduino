e6cad42abf1abf9558da8771b17e04e425454e6c
CelDarley <99372966+CelDarley@users.noreply.github.com>
2024-05-21 21:56:01 -0300

    Update gigaUmaPlaca.ino

--- gigaUmaPlaca.ino
+++ gigaUmaPlaca.ino
@@ -1,9 +1,13 @@
 
-const int pinPwm1 = 3; // pino onde sai o sinal PWM
 const int pinLer1 = A0; // pino que lê o sinal PWM
-
-const int pinPwm2 = 5; // pino onde sai o sinal PWM
 const int pinLer2 = A1; // pino que lê o sinal PWM
+const int pinLer1RX = A2; // pino que lê o sinal PWM
+const int pinLer2TX = A3; // pino que lê o sinal PWM
+
+const int pinPwm1 = 3; // pino onde sai o sinal PWM
+const int pinPwm1RX = 4; // pino onde sai o sinal PWM para RX (BR)
+const int pinPwm2 = 5; // pino onde sai o sinal PWM
+const int pinPwm2TX = 6; // pino onde sai o sinal PWM para TX (AZ)
 
 const int buzzer = 2; 
 
@@ -17,8 +21,8 @@ const long interval2 = 180; //4 HZ
 
 const int pinoAzul = 7;
 const int pinoVermelho = 8;
- 
-//int pinoB = 9; 
+const int ledRX = 9;
+const int ledTX = 10;
 
 bool alterado = false;
 
@@ -27,16 +31,22 @@ void setup() {
 
   pinMode(pinoAzul, OUTPUT); 
   pinMode(pinoVermelho, OUTPUT); 
+  pinMode(ledRX, OUTPUT); 
+  pinMode(ledTX, OUTPUT);
 //
 
   pinMode(buzzer, OUTPUT);
   
   pinMode(pinPwm1,OUTPUT); // Envia o sinal PWM
   pinMode(pinPwm2,OUTPUT); // Envia o sinal PWM
+  pinMode(pinPwm1RX,OUTPUT); // Envia o sinal PWM para fio RX (BR)
+  pinMode(pinPwm2TX,OUTPUT); // Envia o sinal PWM para fio TX (AZ)
   
-  pinMode(pinLer2,INPUT_PULLUP);  // lê o sinal Analógico
   pinMode(pinLer1,INPUT_PULLUP);  // lê o sinal Analógico
- 
+  pinMode(pinLer2,INPUT_PULLUP);  // lê o sinal Analógico
+  pinMode(pinLer1RX,INPUT_PULLUP);  // lê o sinal Analógico para fio RX (BR)
+  pinMode(pinLer2TX,INPUT_PULLUP);  // lê o sinal Analógico para fio TX (AZ)
+  
 }
 void pisca(){
   unsigned long currentMillis = millis();
@@ -53,15 +63,19 @@ void loop() {
 
   analogWrite(pinPwm1,128);// ENVIA EM GND
   analogWrite(pinPwm2,255); // ENVIA EM VCC
+  analogWrite(pinPwm1RX,128);// ENVIA EM RX
+  analogWrite(pinPwm2TX,255); // ENVIA EM TX
   
   int valorGnd = analogRead(pinLer1);// RECEBE DE GND
   int valorVCC = analogRead(pinLer2);// RECEBE DE VCC
+  int valorRX = analogRead(pinLer1RX);// RECEBE DE GND
+  int valorTX = analogRead(pinLer2TX);// RECEBE DE VCC
 
-  valorGnd = analogRead(pinLer1);
   valorGnd = map(valorGnd, 0, 1023, 0, 255);
-
-  valorVCC = analogRead(pinLer2);
   valorVCC = map(valorVCC, 0, 1023, 0, 255);
+  valorRX = map(valorRX, 0, 1023, 0, 255);
+  valorTX = map(valorTX, 0, 1023, 0, 255);
+  
   boolean tocarBuzzer=false;
   
   delay(150);
@@ -77,6 +91,16 @@ void loop() {
           digitalWrite(pinoAzul,LOW);
           tocarBuzzer=true;
         }
+        if(valorRX > 250 ){
+          Serial.println("RX desligado");
+          digitalWrite(ledRX,LOW);
+          tocarBuzzer=true;
+        }
+        if(valorTX > 250){
+          Serial.println("TX desligado");
+          digitalWrite(ledTX,LOW);
+          tocarBuzzer=true;
+        }
         
         // LIGADOS
         Serial.println(valorGnd); // 121
@@ -87,6 +111,16 @@ void loop() {
         Serial.println(valorVCC); // 72
         if((valorVCC > 60) && (valorVCC < 90)){
           digitalWrite(pinoVermelho,HIGH);
+          digitalWrite(buzzer, LOW);
+        }
+        Serial.println(valorRX); // 121
+        if((valorRX > 100)&&(valorRX < 150)){
+          digitalWrite(ledRX,HIGH);
+          digitalWrite(buzzer, LOW);
+        }
+        Serial.println(valorTX); // 72
+        if((valorTX > 60) && (valorTX < 90)){
+          digitalWrite(ledTX,HIGH);
           digitalWrite(buzzer, LOW);
         }
 
@@ -102,7 +136,16 @@ void loop() {
           tocarBuzzer=true;
           pisca();
         }
-
+        if((valorRX > 100)&&(valorRX < 150)){
+          Serial.println("RX INVERTIDO");
+          tocarBuzzer=true;
+          pisca();
+        }
+        if((valorTX > 60) && ( valorTX < 90)){
+          Serial.println("TX INVERTIDO");
+          tocarBuzzer=true;
+          pisca();
+        }
       if(tocarBuzzer==true){
         digitalWrite(buzzer, HIGH);
       }
