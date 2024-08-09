ba1ebbf62e7f3472e3bf2348076dc9c03fdb9d81
darleydias <darlley@gmail.com>
2024-08-09 10:32:02 -0300

    acrescimo teste inversao

---
+++ fingerEnroll.ino
@@ -0,0 +1,214 @@
+/*************************************************** 
+  This is an example sketch for our optical Fingerprint sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> http://www.adafruit.com/products/751
+
+  These displays use TTL Serial to communicate, 2 pins are required to 
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Adafruit_Fingerprint.h>
+
+// On Leonardo/Micro or others with hardware serial, use those! #0 is green wire, #1 is white
+// uncomment this line:
+// #define mySerial Serial1
+
+// For UNO and others without hardware serial, we must use software serial...
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+// comment these two lines if using hardware serial
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3);
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+uint8_t id;
+
+void setup()  
+{
+  Serial.begin(9600);
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  Serial.println("\n\nAdafruit Fingerprint sensor enrollment");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Sensor encontrado!");
+  } else {
+    Serial.println("Sensor não encontrado :(");
+    while (1) { delay(1); }
+  }
+}
+
+uint8_t readnumber(void) {
+  uint8_t num = 0;
+  
+  while (num == 0) {
+    while (! Serial.available());
+    num = Serial.parseInt();
+  }
+  return num;
+}
+
+void loop()                     // run over and over again
+{
+  Serial.println("Proto para cadatrar um dedo!");
+  Serial.println("Digite um número (de 1 a 127)");
+  id = readnumber();
+  if (id == 0) {// ID #0 not allowed, try again!
+     return;
+  }
+  Serial.print("Nr Cadastrado");
+  Serial.println(id);
+  
+  while (!getFingerprintEnroll() );
+}
+
+uint8_t getFingerprintEnroll() {
+
+  int p = -1;
+  Serial.print("Esperando para validar dedo para cadastrar como "); 
+  Serial.println(id);
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Imagem capturada");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Erro de comunicação");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Erro na imagem");
+      break;
+    default:
+      Serial.println("Erro desconhecido");
+      break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(1);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Imagem convertida");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  Serial.println("Remova o dedo");
+  delay(2000);
+  p = 0;
+  while (p != FINGERPRINT_NOFINGER) {
+    p = finger.getImage();
+  }
+  Serial.print("ID "); Serial.println(id);
+  p = -1;
+  Serial.println("Coloque algum dedo novamente");
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.print(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      break;
+    default:
+      Serial.println("Unknown error");
+      break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(2);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  Serial.print("Criando modelo para ");  Serial.println(id);
+  
+  p = finger.createModel();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Prints matched!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_ENROLLMISMATCH) {
+    Serial.println("Fingerprints did not match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  Serial.print("ID "); Serial.println(id);
+  p = finger.storeModel(id);
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Gravado com sucesso!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Não pode gravar nesta localização");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+}
---
+++ gigaUmaPlaca.ino
@@ -0,0 +1,180 @@
+
+const int pinLer1 = A0; // pino que lê o sinal PWM
+const int pinLer2 = A1; // pino que lê o sinal PWM
+const int pinLer1RX = A2; // pino que lê o sinal PWM
+const int pinLer2TX = A3; // pino que lê o sinal PWM
+
+const int pinPwm1 = 3; // pino onde sai o sinal PWM
+const int pinPwm1RX = 4; // pino onde sai o sinal PWM para RX (BR)
+const int pinPwm2 = 5; // pino onde sai o sinal PWM
+const int pinPwm2TX = 6; // pino onde sai o sinal PWM para TX (AZ)
+
+const int buzzer = 2; 
+
+unsigned long previousMillis = 0; 
+unsigned long previousMillis2 = 0; 
+unsigned long previousMillis3 = 0; 
+
+
+const long interval = 60; //4 HZ
+const long interval2 = 180; //4 HZ
+
+const int pinoAzul = 7;
+const int pinoVermelho = 8;
+const int ledRX = 9;
+const int ledTX = 10;
+
+bool alterado = false;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(pinoAzul, OUTPUT); 
+  pinMode(pinoVermelho, OUTPUT); 
+  pinMode(ledRX, OUTPUT); 
+  pinMode(ledTX, OUTPUT);
+//
+
+  pinMode(buzzer, OUTPUT);
+  
+  pinMode(pinPwm1,OUTPUT); // Envia o sinal PWM
+  pinMode(pinPwm2,OUTPUT); // Envia o sinal PWM
+  pinMode(pinPwm1RX,OUTPUT); // Envia o sinal PWM para fio RX (BR)
+  pinMode(pinPwm2TX,OUTPUT); // Envia o sinal PWM para fio TX (AZ)
+  
+  pinMode(pinLer1,INPUT_PULLUP);  // lê o sinal Analógico
+  pinMode(pinLer2,INPUT_PULLUP);  // lê o sinal Analógico
+  pinMode(pinLer1RX,INPUT_PULLUP);  // lê o sinal Analógico para fio RX (BR)
+  pinMode(pinLer2TX,INPUT_PULLUP);  // lê o sinal Analógico para fio TX (AZ)
+  
+}
+void pisca(){
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis; 
+    digitalWrite(ledRX, !digitalRead(pinoVermelho)); 
+  }
+  if (currentMillis - previousMillis2 >= interval2) {
+    previousMillis2 = currentMillis;
+    digitalWrite(ledTX, !digitalRead(pinoAzul));
+  }
+ }
+
+
+
+void piscaTXRX(){
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis; 
+    digitalWrite(pinoVermelho, !digitalRead(pinoVermelho)); 
+  }
+  if (currentMillis - previousMillis2 >= interval2) {
+    previousMillis2 = currentMillis;
+    digitalWrite(pinoAzul, !digitalRead(pinoAzul));
+  }
+ }
+
+
+
+
+ 
+void loop() {
+
+  analogWrite(pinPwm1,128);// ENVIA EM GND
+  analogWrite(pinPwm2,255); // ENVIA EM VCC
+  analogWrite(pinPwm1RX,128);// ENVIA EM RX
+  analogWrite(pinPwm2TX,255); // ENVIA EM TX
+  
+  int valorGnd = analogRead(pinLer1);// RECEBE DE GND
+  int valorVCC = analogRead(pinLer2);// RECEBE DE VCC
+  int valorRX = analogRead(pinLer1RX);// RECEBE DE GND
+  int valorTX = analogRead(pinLer2TX);// RECEBE DE VCC
+
+  valorGnd = map(valorGnd, 0, 1023, 0, 255);
+  valorVCC = map(valorVCC, 0, 1023, 0, 255);
+  valorRX = map(valorRX, 0, 1023, 0, 255);
+  valorTX = map(valorTX, 0, 1023, 0, 255);
+  
+  boolean tocarBuzzer=false;
+  
+  delay(150);
+        // DESLIGADOS
+         
+        if(valorGnd > 250 ){
+          Serial.println("gnd desligado");
+          digitalWrite(pinoVermelho,LOW);
+          tocarBuzzer=true;
+        }
+        if(valorVCC > 250){
+          Serial.println("vcc desligado");
+          digitalWrite(pinoAzul,LOW);
+          tocarBuzzer=true;
+        }
+        if(valorRX > 250 ){
+          Serial.println("RX desligado");
+          digitalWrite(ledRX,LOW);
+          tocarBuzzer=true;
+        }
+        if(valorTX > 250){
+          Serial.println("TX desligado");
+          digitalWrite(ledTX,LOW);
+          tocarBuzzer=true;
+        }
+        
+        // LIGADOS
+        Serial.println(valorGnd); // 121
+        if((valorGnd > 100)&&(valorGnd < 150)){
+          digitalWrite(pinoAzul,HIGH);
+          digitalWrite(buzzer, LOW);
+        }
+        Serial.println(valorVCC); // 72
+        if((valorVCC > 60) && (valorVCC < 90)){
+          digitalWrite(pinoVermelho,HIGH);
+          digitalWrite(buzzer, LOW);
+        }
+        Serial.println(valorRX); // 121
+        if((valorRX > 100)&&(valorRX < 150)){
+          digitalWrite(ledRX,HIGH);
+          digitalWrite(buzzer, LOW);
+        }
+        Serial.println(valorTX); // 72
+        if((valorTX > 60) && (valorTX < 90)){
+          digitalWrite(ledTX,HIGH);
+          digitalWrite(buzzer, LOW);
+        }
+
+        // INVERTIDOS 
+
+        if((valorVCC > 100)&&(valorVCC < 150)){
+          Serial.println("gnd INVERTIDO");
+          tocarBuzzer=true;
+          piscaTXRX();
+        }
+        if((valorGnd > 60) && ( valorGnd < 90)){
+          Serial.println("vcc INVERTIDO");
+          tocarBuzzer=true;
+          piscaTXRX();
+        }
+
+      
+
+        if((valorRX > 60) && ( valorRX < 90)){
+          Serial.println("RX INVERTIDO");
+          tocarBuzzer=true;
+          pisca();
+        }
+
+        if((valorTX > 100)&&(valorTX < 150)){
+          digitalWrite(ledRX,HIGH);
+          digitalWrite(buzzer, LOW);
+        }
+
+
+
+
+        
+
+      if(tocarBuzzer==true){
+        digitalWrite(buzzer, HIGH);
+      }
+}
